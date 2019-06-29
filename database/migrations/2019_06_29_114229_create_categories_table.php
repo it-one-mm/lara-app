219ada9fefb2dd6a2f35a1c5bf55a8f058c29653
it-one-mm <itonemm@gmail.com>
2019-06-29 18:21:25 +0630

    Create Category Migration with Model

--- database/migrations/2019_06_29_114229_create_categories_table.php
+++ database/migrations/2019_06_29_114229_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,9 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        if(Schema::hasTable('posts')) return;
-
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title', 255);
-            $table->text('content');
-            $table->string('slug')->nullable();
-            $table->tinyInteger('status')->default(1);
-            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -33,6 +27,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
     }
 }
